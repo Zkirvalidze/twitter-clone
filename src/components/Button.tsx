4b7814cd0ae092a81baaf3d559c9338a038b2160
zKirvalidze <geostreamer@gmail.com>
2023-04-09 13:21:42 +0400

    added reusable button components

---
+++ src/components/Button.tsx
@@ -0,0 +1,59 @@
+import React, { ButtonHTMLAttributes, Children } from 'react';
+import { cn } from '@/lib/utils';
+
+import { cva, type VariantProps } from 'class-variance-authority';
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  label: string;
+  secondary?: boolean;
+  large?: boolean;
+  onClick: () => void;
+  disabled: boolean;
+  outline?: boolean;
+  fullWidth?: boolean;
+}
+
+const buttonVariants = cva(
+  'disabled:opacity-70 disabled:cursor-not-allowed rounded-full font-semibold hover:opacity-80 transition border-2',
+  {
+    variants: {
+      variant: {
+        default: 'bg-sky-500 text-white border-sky-500',
+        secondary: 'bg-white text-black border-black',
+        outline: ' bg-transparant border-white text-white',
+      },
+      size: {
+        default: 'text-md px-4 py-2',
+        large: 'text-xl px-5 py-3',
+      },
+      width: {
+        default: 'w-fit',
+        fullWidth: 'w-full',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+);
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ label, className,children, disabled, onClick, variant, size }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {label}
+        {children}
+      </button>
+    );
+  }
+);
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
